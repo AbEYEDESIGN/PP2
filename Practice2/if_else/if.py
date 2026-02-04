f9c8473cc0b143ed0cbf2f5e4788ec81cbc53859
AbEYEDESIGN <dzumagalievabay16@gmail.com>
2026-02-04 05:05:02 +0500

    Добавил файлы boolean , if else , loops

---
+++ Practice2/if_else/if.py
@@ -0,0 +1,36 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+#If statement, without indentation (will raise an error):
+
+a = 33
+b = 200
+if b > a:
+print("b is greater than a") # you will get an error
+
+
+# Using a boolean variable:
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+
+# Multiple statements in an if block:
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+
+
+#Checking if a number is positive:
+
+number = 15
+if number > 0:
+  print("The number is positive")
---
+++ Practice2/if_else/shorthand_if.py
@@ -0,0 +1,44 @@
+# One-line if statement:
+
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+
+# You still need the colon : after the condition.
+
+
+# One-line if/else that prints a value:
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+# You can also use a one-line if/else to choose a value and assign it to a variable:
+
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+
+# The syntax follows this pattern:
+
+variable = value_if_true if condition else value_if_false
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+
+# Finding the maximum of two numbers:
+x = 15
+y = 20
+max_value = x if x > y else y
+print("Maximum value:", max_value)
+
+# Setting a default value:
+
+username = ""
+display_name = username if username else "Guest"
+print("Welcome,", display_name)
