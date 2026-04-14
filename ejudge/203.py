77959ad85cbaed4245fa15283a866e583e71a526
AbEYEDESIGN <dzumagalievabay16@gmail.com>
2026-04-15 00:52:50 +0500

    Mickey mouse added

--- ejudge/203.py
+++ ejudge/203.py
@@ -1,12 +1,11 @@
-n = int(input())
-su = 1
-i = 1
-while i <= n:
-    su = i * su
-    i+=1
-print(su)
-
-
+class Car: 
+    def __init__(self , brand , year):
+        self.brand = brand
+        self.year = year
+    def display_info(self):
+        print(self.brand , self.year)
+o1=Car("mers " , 2009)
+o1.display_info()
 
 
 
